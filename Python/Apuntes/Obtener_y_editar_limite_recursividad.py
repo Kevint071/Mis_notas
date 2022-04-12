71435e97ab52ea5e6ee999620d61e896a368d0d4
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2022-04-12 18:28:49 -0500

    Añadiendo archivo git_show y limite de recursividad

---
+++ Python/Apuntes/Obtener_y_editar_limite_recursividad.py
@@ -0,0 +1,12 @@
+import sys
+
+# Para  obtener el limite de recursividad se hace lo siguiente
+
+variable = sys.getrecursionlimit()
+print(variable) # 1000
+
+# Para editarlo se hace lo siguiente:
+
+sys.setrecursionlimit(1300) # El límite editado que se puso es 1300
+
+# Esto sirve para cuando necesites hacer algo con un límite de recursividad mayor a 997 ya que python por defecto coloca este número como el límite para que no haya un debordamiento en C y python no se bloquee.
