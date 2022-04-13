e3679fd8629a2d51e5e2e78b78c36e352bc0d118
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2022-04-13 15:47:05 -0500

    Usando punto de entrada en Adivinar numero

--- Python/Programas/Adivinar_numero.py
+++ Python/Programas/Adivinar_numero.py
@@ -1,61 +1,68 @@
 from random import randint
-lista_usuario = [0]
-si_no = 1
 
-while si_no == 1:
 
-    win = None
-    lista_usuario.clear()    
-    numero = randint(1, 30)
-    i = 0
+def run():
+    lista_usuario = [0]
+    si_no = 1
 
-    while i < 7:
-        i += 1
+    while si_no == 1:
+
+        win = None
+        lista_usuario.clear()    
+        numero = randint(1, 30)
+        i = 0
+
+        while i < 7:
+            i += 1
+            while True:
+                try:
+                    num_usuario = int(input("Intenta adivinar el número que pienso entre 1 y 30: "))
+                    lista_usuario.append(num_usuario)
+
+                    numero_menos_1 = numero - 1 in lista_usuario
+                    numero_mas_1 = numero + 1 in lista_usuario
+
+                    if numero_menos_1 == True and numero_mas_1 == True:
+                        print(f"Como ya has elegido el número {numero-1} y el {numero+1} ovbiamente el número que pienso es el {numero} así que has ganado\n")
+                        win = 0
+                        break
+                    
+                    if lista_usuario.count(num_usuario) == 2:
+                        print("Este número ya lo había dicho, piense otro\n")
+                        lista_usuario.remove(num_usuario)
+                    elif num_usuario > 30 or num_usuario < 1:
+                        print("El número que debes adivinar está entre 1 y 30\n")
+                    else:
+                        break 
+                except:
+                    print("Número no válido")
+
+            if numero_menos_1 == True and numero_mas_1 == True:
+                break
+
+            if num_usuario > numero:
+                print("El número en que pienso es mas bajo...\n")
+            elif num_usuario < numero:
+                print("El número en que pienso es mas alto...\n")
+            else:
+                win = "Has acertado!\n"
+                print(win)
+                break
+        if win == None:
+            print(f"Perdistes... El número en que pensaba era el {numero}\n")
+            
         while True:
             try:
-                num_usuario = int(input("Intenta adivinar el número que pienso entre 1 y 30: "))
-                lista_usuario.append(num_usuario)
-
-                numero_menos_1 = numero - 1 in lista_usuario
-                numero_mas_1 = numero + 1 in lista_usuario
-
-                if numero_menos_1 == True and numero_mas_1 == True:
-                    print(f"Como ya has elegido el número {numero-1} y el {numero+1} ovbiamente el número que pienso es el {numero} así que has ganado\n")
-                    win = 0
-                    break
+                si_no = int(input("¿Desea jugar otra vez? 1(si) 2(no): "))
                 
-                if lista_usuario.count(num_usuario) == 2:
-                    print("Este número ya lo había dicho, piense otro\n")
-                    lista_usuario.remove(num_usuario)
-                elif num_usuario > 30 or num_usuario < 1:
-                    print("El número que debes adivinar está entre 1 y 30\n")
+                if si_no == 1 or si_no == 2:
+                    print()
+                    break
                 else:
-                    break 
+                    print("El número no es válido\n")
             except:
-                print("Número no válido")
+                print("Por Dios esto no es un número\n")
 
-        if numero_menos_1 == True and numero_mas_1 == True:
-            break
 
-        if num_usuario > numero:
-            print("El número en que pienso es mas bajo...\n")
-        elif num_usuario < numero:
-            print("El número en que pienso es mas alto...\n")
-        else:
-            win = "Has acertado!\n"
-            print(win)
-            break
-    if win == None:
-        print(f"Perdistes... El número en que pensaba era el {numero}\n")
-        
-    while True:
-        try:
-            si_no = int(input("¿Desea jugar otra vez? 1(si) 2(no): "))
-            
-            if si_no == 1 or si_no == 2:
-                print()
-                break
-            else:
-                print("El número no es válido\n")
-        except:
-            print("Por Dios esto no es un número\n")
+if __name__ == "__main__":
+    run()
