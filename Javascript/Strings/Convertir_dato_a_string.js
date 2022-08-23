39aa4bcf6837cdf1a28da90494e56a9deb3044cc
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2022-08-23 16:02:21 -0500

    Agregando conversion datos number_string y viceversa

---
+++ Javascript/Strings/Convertir_dato_a_string.js
@@ -0,0 +1,7 @@
+// Para convertir un tipo de dato a string se puede hacer lo siguiente:
+
+var numero = 1;
+var num_string = String(numero);
+
+console.log(typeof(num_string)); // string
+console.log(typeof(numero)); // number
---
+++ Javascript/Strings/Convertir_num_a_string.js
@@ -0,0 +1,7 @@
+// Para convertir un string a un numero se hace lo siguiente:
+
+var num_string = "23";
+var numero = Number(num_string);
+
+console.log(typeof(numero)); // number
+console.log(typeof(num_string)); // string
