fee6146bfa2dd6254f543847072aebe250626bb9
Kevin <andrestorrecilla.14@gmail.com>
2023-10-27 16:05:38 -0500

    style(Eliminando saltos de linea en los parametros de las querys)

--- Python/Programas/Programa_aprender_ingles/Conexion_base_de_datos.py
+++ Python/Programas/Programa_aprender_ingles/Conexion_base_de_datos.py
@@ -4,8 +4,7 @@ from credenciales import conn
 def existencia_tabla():
     cursor = conn.cursor()
 
-    cursor.execute(
-        """SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';""")
+    cursor.execute("""SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';""")
     tables = cursor.fetchall()
 
     if ("palabras",) in tables:
@@ -17,14 +16,7 @@ def existencia_tabla():
 
 def crear_tabla():
     cursor = conn.cursor()
-    
-    cursor.execute("""
-  CREATE TABLE palabras(
-    id Serial PRIMARY KEY,
-    palabra text,
-    traduccion text
-  );
-""")
+    cursor.execute("""CREATE TABLE palabras(id Serial PRIMARY KEY, palabra text, traduccion text);""")
 
     # Usar codigo si se quiere ver las tablas creadas
 
