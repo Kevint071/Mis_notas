300086be72f33baa054a73d611f083c6f4d2e0c0
Kevin <andrestorrecilla.14@gmail.com>
2023-10-27 15:28:45 -0500

    feat(new repo for projects of python and upgrade some scripts)

--- Python/Programas/Adivinar_numero.py
+++ Python/Programas/Adivinar_numero.py
@@ -1,12 +1,78 @@
 from random import randint
+from os import system, name
 
+
+def limpiar_pantalla():
+    if name == "posix":
+        system("clear")
+    elif name == "nt" or name == "dos" or name == "ce":
+        system("cls")
+
+
+def adivinar_numero(lista_usuario):
+    # Se digita el numero y en caso de errores se mandan mensajes a la consola
+    while True:
+        try:
+            num_usuario = int(input("Intenta adivinar el número que pienso entre 1 y 30: "))
+            lista_usuario.append(num_usuario)
+            
+            if lista_usuario.count(num_usuario) == 2:
+                print("Este número ya lo había dicho, piense otro\n")
+                lista_usuario.remove(num_usuario)
+            elif num_usuario > 30 or num_usuario < 1:
+                print("El número que debes adivinar está entre 1 y 30\n")
+            else:
+                break 
+        except:
+            print("Número no válido")
+    return num_usuario
+
+
+def comparar_numero(num_usuario, numero):
+    win = False
+    if num_usuario > numero:
+        print("El número en que pienso es mas bajo...\n")
+    elif num_usuario < numero:
+        print("El número en que pienso es mas alto...\n")
+    else:
+        print("Has acertado!\n")
+        win = True
+    return win
+
+
+def volver_a_jugar():
+    while True:
+        try:
+            desicion = int(input("¿Desea jugar otra vez? 1(si) 2(no): "))
+
+            if desicion == 1 or desicion == 2:
+                print()
+                break
+            else:
+                print("El número no es válido\n")
+        except:
+            print("Esto no es un número\n")
+    return desicion
+
+
+def inicio_juego(lista_usuario, numero, i):
+    while i < 6:
+        i += 1
+        num_usuario = adivinar_numero(lista_usuario)
+
+        # Se dice si el número digitado por el usuario es mayor o menor o igual
+
+        win = comparar_numero(num_usuario, numero)
+        if win == True:
+            return win
+    return win
 
 def run():
     lista_usuario = []
-    si_no = 1
+    desicion = 1
 
-    while si_no == 1:
-
+    while desicion == 1:
+        limpiar_pantalla()
         win = None
         lista_usuario.clear()    
         numero = randint(1, 30)
@@ -14,53 +80,14 @@ def run():
 
         # Se crea el ciclo while con 6 intentos, representados por la variable i
 
-        while i < 6:
-            i += 1
+        win = inicio_juego(lista_usuario, numero, i)
 
-            # Se digita el numero y en caso de errores se mandan mensajes a la consola
-
-            while True:
-                try:
-                    num_usuario = int(input("Intenta adivinar el número que pienso entre 1 y 30: "))
-                    lista_usuario.append(num_usuario)
-                    
-                    if lista_usuario.count(num_usuario) == 2:
-                        print("Este número ya lo había dicho, piense otro\n")
-                        lista_usuario.remove(num_usuario)
-                    elif num_usuario > 30 or num_usuario < 1:
-                        print("El número que debes adivinar está entre 1 y 30\n")
-                    else:
-                        break 
-                except:
-                    print("Número no válido")
-
-            # Se dice si el número digitado por el usuario es mayor o menor o igual
-
-            if num_usuario > numero:
-                print("El número en que pienso es mas bajo...\n")
-            elif num_usuario < numero:
-                print("El número en que pienso es mas alto...\n")
-            else:
-                win = "Has acertado!\n"
-                print(win)
-                break
-
-        if win == None:
+        if win == False:
             print(f"Perdistes... El número en que pensaba era el {numero}\n")
         
         # En caso de querer volver a jugar
-            
-        while True:
-            try:
-                si_no = int(input("¿Desea jugar otra vez? 1(si) 2(no): "))
-                
-                if si_no == 1 or si_no == 2:
-                    print()
-                    break
-                else:
-                    print("El número no es válido\n")
-            except:
-                print("Por Dios esto no es un número\n")
+
+        desicion = volver_a_jugar()
 
 
 if __name__ == "__main__":
--- Python/Programas/Cantidad_aleatorio_suma_10.py
+++ Python/Programas/Cantidad_aleatorio_suma_10.py
@@ -1,11 +1,6 @@
 import random
 
-lista = []
-
-for i in range(249):
-    num = random.randint(0, 9)
-    lista.append(str(num))
-
+lista = [str(random.randint(0, 9)) for i in range(250)]
 acum = 0
 
 suma_10 = []
@@ -21,7 +16,8 @@ for i in range(0, len(lista)):
         acum += 1
         suma_10.append([int(lista[i]), int(lista[i+1])])
         
-print(f"\n{''.join(lista)}\n")
+# print(f"\n{''.join(lista)}\n")
+[print(f"{i+1}. {valor}") for i, valor in enumerate(lista)]
 print(f"La cantidad de sumas que hay son: {acum}\n")
 print(f"Lista de las sumas: {suma_10}\n")
 
